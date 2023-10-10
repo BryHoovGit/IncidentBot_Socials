616863f104d7068d6cf85e4fa2f0f661e7602b87
Bryce <86072073+BryHoovGit@users.noreply.github.com>
2023-10-09 23:43:46 -0500

    Refactor

--- redditBot_CU.py
+++ redditBot_CU.py
@@ -7,82 +7,73 @@ import os
 
 # Load the environment variables
 dotenv.load_dotenv()
-# Get the webhook URL from the environment variables
+
+# Get the webhook URL and subreddit name from the environment variables
 webhook_url = os.getenv("WEBHOOK_URL")
-phrases = os.getenv("PHRASES").split(",")
+subreddit_name = os.getenv("SUBREDDIT_NAME")
+
 # Create a Reddit instance
 reddit = praw.Reddit(client_id=os.getenv("CLIENT_ID"),
                      client_secret=os.getenv("CLIENT_SECRET"),
                      user_agent=os.getenv("USER_AGENT"))
 
-
-def isOfficeHours():
-    date_format = '%m_%d_%Y_%H_%M_%S_%Z'
-    # Get the current time in PST.
-    date = datetime.datetime.now(tz=pytz.timezone('UTC'))
-    date = date.astimezone(pytz.timezone('US/Pacific'))
-    # Get the PST date and time in the specified format.
-    pst_datetime = date.strftime(date_format)
-    # Parse the hour value from the formatted string.
-    hour = int(pst_datetime.split('_')[3])
-    # Format the PST date and time into the desired format.
-    formatted_datetime = date.strftime('%m/%d/%Y %H:%M PST')
-    print(formatted_datetime)
-
-    # Check if the time is between 9am and 5pm in PST.
-    if hour >= 9 and hour < 17:
-        return True
-    else:
-        return False
-
-
-if isOfficeHours():
-    print('Inside office hours in PST.')
-else:
-    print('Outside office hours in PST.')
-
 # Get the subreddit
-subreddit = reddit.subreddit(os.getenv("SUBREDDIT_NAME"))
+subreddit = reddit.subreddit(subreddit_name)
+
 # Get the last 10 posts from the subreddit
 posts = subreddit.new(limit=10)
 
 # Create a list of the phrases you want to search for
 phrases = ["outage", "down", "can't login", "infinite loading", "Outage"]
-# Create an array to store the post IDs
-post_ids = []
 
 
 def check_post_for_phrases(post, phrases):
     """Checks a post for one of the phrases in either post.title or post.selftext.
+
     Args:
-      post: A PRAW submission object.
-      phrases: A list of strings to search for.
+        post: A PRAW submission object.
+        phrases: A list of strings to search for.
+
     Returns:
-      True if the post contains one of the phrases, False otherwise.
+        True if the post contains one of the phrases, False otherwise.
     """
+
     for phrase in phrases:
         if phrase in post.title or phrase in post.selftext:
             return True
     return False
 
 
-for i, post in enumerate(posts):
+def is_office_hours():
+    """Checks if it is currently office hours in Pacific Standard Time (PST)."""
+
+    date_format = '%m_%d_%Y_%H_%M_%S_%Z'
+    now = datetime.datetime.now(tz=pytz.timezone('UTC'))
+    pst_now = now.astimezone(pytz.timezone('US/Pacific'))
+    pst_datetime = pst_now.strftime(date_format)
+    hour = int(pst_datetime.split('_')[3])
+
+    return hour >= 9 and hour < 17
+
+
+for post in posts:
     if check_post_for_phrases(post, phrases):
-        post_ids.append(post.id)
         # Create a POST request to webhook.site
         payload = {
             "post_id": post.id,
             "post_title": post.title,
             "post_url": post.url,
-            "post_text": post.selftext
+            "post_text": post.selftext,
+            "is_office_hours": is_office_hours()
         }
         headers = {
             "Content-Type": "application/json"
         }
         response = requests.post(webhook_url, json=payload, headers=headers)
+
         # Check if the POST request was successful
         if response.status_code == 200:
-            print("POST request to webhook.site was successful.")
+            print(f"POST request to {webhook_url} was successful.")
         else:
             print(
-                f"POST request to webhook.site failed with status code {response.status_code}.")
+                f"POST request to {webhook_url} failed with status code {response.status_code}.")
