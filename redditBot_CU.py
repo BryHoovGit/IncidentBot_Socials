5c4ac9e02f3ac6b749012cb29590a11d2ef13aaa
Bryce Hoover <bryhoover11@gmail.com>
2023-10-24 01:26:10 -0500

    Refactor aimed at size reduction for lambda

--- redditBot_CU.py
+++ redditBot_CU.py
@@ -1,10 +1,14 @@
 import praw
-import requests
-import datetime
-import pytz
+import dateparser
+import json
 import os
+import boto3
+import dotenv
 
-webhook_url = os.environ.get("WEBHOOK_URL")
+# Load the environment variables
+dotenv.load_dotenv()
+
+webhook = os.environ.get("WEBHOOK_URL")
 subreddit_name = os.environ.get("SUBREDDIT_NAME")
 phrases = os.environ.get("PHRASES")
 
@@ -12,62 +16,32 @@ phrases = os.environ.get("PHRASES")
 reddit = praw.Reddit(client_id=os.environ.get("CLIENT_ID"),
                      client_secret=os.environ.get("CLIENT_SECRET"),
                      user_agent=os.environ.get("USER_AGENT"))
-
 # Get the subreddit
 subreddit = reddit.subreddit(subreddit_name)
-
 # Get the last 10 posts from the subreddit
 posts = subreddit.new(limit=10)
 
-# Get the phrases you want to search for from the environment variable
-phrases = [phrase.strip("[]").replace('"', '')
-           for phrase in phrases.split(",")]
-
 
 def check_post_for_phrases(post, phrases):
-    """Checks a post for one of the phrases in either post.title or post.selftext.
-
-    Args:
-        post: A PRAW submission object.
-        phrases: A list of strings to search for.
-
-    Returns:
-        True if the post contains one of the phrases, False otherwise.
-    """
-
+    # True if the post contains one of the phrases, False otherwise.
     for phrase in phrases:
         if phrase in post.title or phrase in post.selftext:
             return True
     return False
 
 
-def is_office_hours():
-    """Checks if it is currently office hours in Pacific Standard Time (PST).
-
-    Returns:
-        True if it is currently office hours, False otherwise.
-    """
-
-    # Get the current time in UTC
-    now = datetime.datetime.now(tz=pytz.timezone('UTC'))
-
-    # Convert the current time to PST
-    pst_now = now.astimezone(pytz.timezone('US/Pacific'))
-
-    # Get the hour of the day in PST
-    hour = pst_now.hour
-
-    # Return True if it is currently between 9am and 5pm in PST
+def is_office_hours(now=dateparser.parse('now', settings={'TIMEZONE': 'US/Pacific'})):
+    # True if it is currently office hours, False otherwise.
+    hour = now.hour
+    print(hour)
     return hour >= 9 and hour < 17
 
 
 def lambda_handler(event, context):
     print(f"🕑 Triggered during office hours: {is_office_hours()}🕑")
-    print(f"🕵️‍♀️ Searching last 10 posts to /r/{subreddit}.🕵️‍♀️")
-    """AWS Lambda function handler."""
-
+    print(f"🕵️‍♀️ Searching last 10 posts to /r/{subreddit_name}.🕵️‍♀️")
     # Check each post for the specified phrases and send a POST request to webhook.site if one is found
-    for post in posts:
+    for post in subreddit.new(limit=10):
         if check_post_for_phrases(post, phrases):
             # Log a message to the console
             print(f'✅ Found match.✅')
@@ -78,19 +52,24 @@ def lambda_handler(event, context):
                 "post_title": post.title,
                 "post_url": post.url,
                 "post_text": post.selftext,
-                "is_office_hours": is_office_hours()
+                "is_office_hours": is_office_hours(),
             }
             headers = {
-                "Content-Type": "application/json"
+                "Content-Type": "application/json",
             }
-            response = requests.post(
-                webhook_url, json=payload, headers=headers)
+            # send the POST request
+            client = boto3.client('apigatewaymanagementapi')
+            response = client.post_to_connection(
+                ConnectionId=webhook,
+                Data=json.dumps(payload, indent=4),
+                Headers=headers,
+            )
             print(f"📤 Sending match to webhook.📤")
             # Check if the POST request was successful
-            if response.status_code == 200:
-                print(f"👍 POST request to {webhook_url} was successful.👍")
+            if response['StatusCode'] == 200:
+                print(f"👍 POST request to {webhook} was successful.👍")
             else:
                 print(
-                    f"👎 POST request to {webhook_url} failed with status code {response.status_code}.👎")
+                    f"👎 POST request to {webhook} failed with status code {response['StatusCode']}.👎")
         else:
             print(f'❌ {post.title}❌')
